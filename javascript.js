97baaec434a8e80060de1641447ac31e6e2cc682
David Fairbairn <130714363+GitDavidHub@users.noreply.github.com>
2023-11-29 01:24:34 -0500

    html css and java finished in very rough draft

--- javascript.js
+++ javascript.js
@@ -1,2 +1,55 @@
+const etchASketch = document.getElementById('etchASketch');
+const gridSizeSlider = document.getElementById('gridSizeSlider');
+const gridSizeDisplay = document.getElementById('gridSizeDisplay');
+let isDrawing = false;
+let currentColor = 'black';
 
+function createGrid() {
+    const gridSize = gridSizeSlider.value;
+    etchASketch.innerHTML = '';
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.addEventListener('mouseover', draw);
+        gridCell.addEventListener('mousedown', startDrawing);
+        etchASketch.appendChild(gridCell);
+    }
+    gridSizeDisplay.textContent = 'Grid Size: ${gridSize}x${gridSize}';
+}
+
+function draw () {
+    if (isDrawing) {
+        this.style.backgroundColor = currentColor;
+    }
+}
+
+function startDrawing() {
+    isDrawing = true;
+    draw.call(this);
+}
+
+function setColor() {
+    currentColor = prompt('Enter a color:');
+}
+
+function setEraser() {
+    currentColor = 'white';
+}
+
+function resetBoard() {
+    const gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    });
+}
+
+function updateGridSize() {
+    createGrid();
+}
+
+
+document.addEventListener('mouseup', () => isDrawing = false);
+
+createGrid();
 
